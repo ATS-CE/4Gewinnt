7dc5b0dbeb90f0bbfeb184937ab86edc05b6c002
BmJngBmGl <desklerjr@outlook.de>
2025-10-06 13:47:36 +0200

    Added Gameloop and implemented random starting Player

--- 4gewinnt.py
+++ 4gewinnt.py
@@ -1,10 +1,11 @@
 import random
-import time
 
-startingPlayer = random.randint(1,2)
 print("4 Gewinnt")
 print("Die Leisten sind von 0-6 durchnummeriert.\nWenn du dran bist gib eine Zahl an, dort wird dann dein O sein.")
 print("Viel Glück\n"+20*"-")
+
+playeroptions = ["X", "O"]
+player = random.choice(playeroptions)
 
 ROWS = 6
 COLS = 7
@@ -23,7 +24,7 @@ def dropPiece(col, piece):
             return True
     return False
 
-def checkWinner():
+def checkWinner(piece):
     for r in range(ROWS):
         for c in range(COLS - 3):
             if all(board[r][c+i] == piece for i in range(4)):
@@ -37,10 +38,15 @@ def checkWinner():
 def boardFull():
     return all(board[0][c]!= " " for c in range (COLS))
 
-player = "X"
+
 while True:
-    
-
-
-
-
+    printBoard()
+    col = int(input(f"Spieler {player} wähle eine Spalte (0-{COLS-1})\n> "))
+    if col < 0 or col >= COLS or not dropPiece(col, player):
+        print("Ungültige Eingabe, erneut")
+        continue
+    if checkWinner(player):
+        printBoard()
+        print(f"Spieler {player} hat gewonnen!")
+        break
+    player = "O" if player == "X" else "X"
